783f82ad4e60b217fb8b9f45c855e76922e4b3a8
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2024-01-09 20:18:15 +0300

    learn about ternary and exponential search

--- SearchingAlgorithms/Program.cs
+++ SearchingAlgorithms/Program.cs
@@ -35,10 +35,33 @@
             }
 
             //тернарный поиск
+            int ternarySearchResult = TernarySearch(array, target);
+
+            if (ternarySearchResult != -1)
+            {
+                Console.WriteLine($"Элемент {target} найден в массиве по индексу {result}.");
+            }
+            else
+            {
+                Console.WriteLine($"Элемент {target} не найден в массиве.");
+            }
+
+            //экспоненциальный поиск
+            int exponentialySearchResult = ExponentialSearch(array, target);
+
+            if (exponentialySearchResult != -1)
+            {
+                Console.WriteLine($"Элемент {target} найден в массиве по индексу {result}.");
+            }
+            else
+            {
+                Console.WriteLine($"Элемент {target} не найден в массиве.");
+            }
         }
 
         static int LinearSearch(int[] array, int target)
         {
+            Console.WriteLine("Линейный поиск ");
             int counter = 0;
             for (int i = 0; i < array.Length; i++)
             {
@@ -52,9 +75,9 @@
             Console.WriteLine("Количество шагов: " + counter);
             return -1; 
         }
-
         static int BinarySearch(int[] array, int target)
         {
+            Console.WriteLine("Бинарный поиск ");
             int counter = 0;
 
             int left = 0;
@@ -81,6 +104,93 @@
             }
             Console.WriteLine("Количество шагов: " + counter);
             return -1; 
+        }
+        static int TernarySearch(int[] array, int target)
+        {
+            Console.WriteLine("Тернарный поиск ");
+            int counter = 0;
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                counter++;
+                int partitionSize = (right - left) / 3;
+                int mid1 = left + partitionSize;
+                int mid2 = right - partitionSize;
+
+                if (array[mid1] == target)
+                {
+                    Console.WriteLine("Количество шагов: " + counter);
+                    return mid1;
+                }
+                else if (array[mid2] == target)
+                {
+                    Console.WriteLine("Количество шагов: " + counter);
+                    return mid2;
+                }
+                else if (target < array[mid1])
+                {
+                    right = mid1 - 1;
+                }
+                else if (target > array[mid2])
+                {
+                    left = mid2 + 1;
+                }
+                else
+                {
+                    left = mid1 + 1;
+                    right = mid2 - 1;
+                }
+            }
+
+            Console.WriteLine("Количество шагов: " + counter);
+            return -1; 
+        }
+
+        //экспоненциальный поиск
+        static int BinarySearch(int[] array, int target, int left, int right)
+        {
+            int counter = 0;
+            while (left <= right)
+            {
+                counter++;
+                int mid = left + (right - left) / 2;
+
+                if (array[mid] == target)
+                {
+                    Console.WriteLine("Количество шагов: " + counter);
+                    return mid; // Возвращаем индекс элемента, если он найден
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            Console.WriteLine("Количество шагов: " + counter);
+            return -1; // Возвращаем -1, если элемент не найден
+        }
+
+        static int ExponentialSearch(int[] array, int target)
+        {
+            Console.WriteLine("Экспоненциальный поиск ");
+            int bound = 1;
+
+            while (bound < array.Length && array[bound] < target)
+            {
+                bound *= 2;
+            }
+
+            int left = bound / 2;
+            int right = Math.Min(bound, array.Length - 1);
+
+            return BinarySearch(array, target, left, right);
         }
     }
 
